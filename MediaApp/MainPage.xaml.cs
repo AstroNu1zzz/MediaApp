2522f864bf290d8c243493525d8ef7b4e804a74b
AstroNu1zzz <nicolassepulveda.lopez2009@gmail.com>
2026-09-18 19:55:47 -0600

    feat(assets): add icons, custom fonts, audio, and video files

--- MediaApp/MainPage.xaml.cs
+++ MediaApp/MainPage.xaml.cs
@@ -1,20 +1,43 @@
-﻿namespace MediaApp
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MediaApp
+
 {
     public partial class MainPage : ContentPage
     {
-
+        private bool _isPlaying = false;
         public MainPage()
         {
             InitializeComponent();
         }
 
+
         private async void VerVideoClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync($"{nameof(Video)}");
-        }
-        private async void AudioBtnClicked(object sender, EventArgs e)
-        {
+            if (_isPlaying)
+            {
+                AudioPlayer.Pause();
+                _isPlaying = false;
+                AudioBtn.Text = "Audio";
+            }
 
+            await Navigation.PushModalAsync(new Video());
+        }
+        private void AudioBtnClicked(object sender, EventArgs e)
+        {
+            if (_isPlaying)
+            {
+                AudioPlayer.Pause();
+                _isPlaying = false;
+                AudioBtn.Text = "Audio";
+            }
+            else
+            {
+                AudioPlayer.Play();
+                _isPlaying = true;
+                AudioBtn.Text = "Pausar audio";
+            }
+        
         }
     }
 }
